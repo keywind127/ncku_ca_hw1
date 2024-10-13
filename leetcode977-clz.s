1eb58c31371f9a226eb49908238ccfcdf613c91f
Kevin S. <68499290+keywind127@users.noreply.github.com>
2024-10-14 01:59:44 +0800

    perf: compute_bytes_needed speedup
    Optimized the looping process by shifting iterators by 4 each time rather than repeatedly performing "shift-left-logical" operations.

--- leetcode977-clz.s
+++ leetcode977-clz.s
@@ -10,18 +10,18 @@
         ecall
     my_clz:
         my_clz_prologue:
-            add t0, x0, a0  # t0 = x
+            add t0, x0, a0    # t0 = x
         my_clz_padding:
-            srli t1, t0, 1  # t1 = x >> 1
-            or t0, t0, t1   # t0 = _x = x | (x >> 1)
-            srli t1, t0, 2  # t1 = _x >> 2
-            or t0, t0, t1   # t0 = __x = _x | (_x >> 2)
-            srli t1, t0, 4  # t1 = __x >> 4
-            or t0, t0, t1   # t0 = ___x = __x | (__x >> 4)
-            srli t1, t0, 8  # t1 = ___x >> 8
-            or t0, t0, t1   # t0 = ____x = ___x | (___x >> 8)
-            srli t1, t0, 16 # t1 = ____x >> 16
-            or t0, t0, t1   # t0 = p = ____x | (____x >> 16)
+            srli t1, t0, 1    # t1 = x >> 1
+            or t0, t0, t1     # t0 = _x = x | (x >> 1)
+            srli t1, t0, 2    # t1 = _x >> 2
+            or t0, t0, t1     # t0 = __x = _x | (_x >> 2)
+            srli t1, t0, 4    # t1 = __x >> 4
+            or t0, t0, t1     # t0 = ___x = __x | (__x >> 4)
+            srli t1, t0, 8    # t1 = ___x >> 8
+            or t0, t0, t1     # t0 = ____x = ___x | (___x >> 8)
+            srli t1, t0, 16   # t1 = ____x >> 16
+            or t0, t0, t1     # t0 = p = ____x | (____x >> 16)
         my_clz_popcount:
             srli t1, t0, 1    # t1 = p >> 1
             li t2, 0x55555555 # t2 = mask1 = 0b0101_0101_0101_0101_0101_0101_0101_0101
@@ -45,7 +45,7 @@
             li t1, 32         # t1 = 32
             sub a0, t1, t0    # a0 = 32 - (v & mask4)
         my_clz_epilogue:
-            jr ra     # return control
+            jr ra             # return control
     compute_bytes_needed:
         compute_bytes_prologue:
             addi sp, sp, -16                      # update stack position to allocate 4 words 
@@ -56,17 +56,17 @@
         compute_bytes_prologue_end:
             mv s0, a0                             # s0 = int_list
             addi s1, a1, -1                       # s1 = idx = n - 1
+            slli s1, s1, 2                        # s1 = 4 * idx = 4 * (n - 1)
             li s2, 32                             # s2 = min_leading_zeros
         compute_bytes_loop:
-            blt s1, x0, compute_bytes_epilogue    # terminate loop if idx < 0
-            slli t0, s1, 2                        # t0 = idx << 2
-            add t0, s0, t0                        # t0 = &int_list + (idx << 2)
+            blt s1, x0, compute_bytes_epilogue    # terminate loop if (idx * 4) < 0
+            add t0, s0, s1                        # t0 = &int_list + (idx * 4)
             lw a0, 0(t0)                          # a0 = int_list[idx]
             jal my_clz                            # a0 = num_leading_zeros
             blt s2, a0, skip_update               # skip updating s2 if s2 < a0
             mv s2, a0                             # s2 = a0 = smaller min_leading_zeros
         skip_update:
-            addi s1, s1, -1                       # s1 = s1 - 1; idx = idx - 1
+            addi s1, s1, -4                       # s1 = s1 - 4; 4 * idx = 4 * idx - 4
             j compute_bytes_loop                  # continue loop
         compute_bytes_epilogue:
             li t0, 32                             # t0 = 32 (constant) 
@@ -77,4 +77,3 @@
             lw ra, 12(sp)                         # pop ra from stack
             addi sp, sp, 16                       # update stack position to deallocate 4 words
             jr ra                                 # return to caller
-        
